95d111055cf8dd61abf9ee21910185afdb464f5a
Seongjin Ahn <sjan137@naver.com>
2021-08-24 00:46:53 +0900

    csv 작성을 위한 모듈 생성

---
+++ 비행기 티켓 금액 스크래핑/usecsv.py
@@ -0,0 +1,15 @@
+import csv
+
+def opencsv(filename):
+    f = open(filename, 'r')
+    reader = csv.reader(f)
+    output = []
+    for i in reader:
+        output.append(i)
+    f.close()
+    return output
+
+def writecsv(filename, the_list):
+    with open(filename, 'w', newline='') as f:
+        a = csv.writer(f, delimiter=',')
+        a.writerows(the_list)

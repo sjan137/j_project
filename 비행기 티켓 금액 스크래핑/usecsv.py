a857848ebc6deac8848cfb48430453cf61761635
Seongjin Ahn <sjan137@naver.com>
2021-08-24 11:51:33 +0900

    usecsv 모듈 점검

--- 비행기 티켓 금액 스크래핑/usecsv.py
+++ 비행기 티켓 금액 스크래핑/usecsv.py
@@ -13,3 +13,6 @@ def writecsv(filename, the_list):
     with open(filename, 'w', newline='') as f:
         a = csv.writer(f, delimiter=',')
         a.writerows(the_list)
+
+# writecsv('비행기 티켓 금액 스크래핑/tickets.csv', [['date', 'day', 'depart', 'the lowest price']])
+# print(opencsv('비행기 티켓 금액 스크래핑/tickets.csv'))

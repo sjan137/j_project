4efe35cf061924486b144890073a40e677d5f0d8
seongjin <sjan137@naver.com>
2022-03-06 20:57:59 +0900

    파일 정리
    잘못 업로드 된 팀 프로젝트 파일 삭제 및 기타 파일 정리

--- yeardream/front_end/static/js/main_index.js
+++
@@ -1,7 +0,0 @@
-import { animate, spring, stagger } from "https://cdn.skypack.dev/motion";
-
-animate(
-  ".box",
-  { y: [200, 0], opacity: 1 },
-  { duration: 5, easing: spring(), delay: stagger(0.5) }
-);
